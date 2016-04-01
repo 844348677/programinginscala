cdbe1beed724c517c913a3812c7a05a7a8044e4c
844348677 <844348677@qq.com>
2016-04-01 21:42:53 +0800

    one

--- src/main/scala/FileMatcher.scala
+++ src/main/scala/FileMatcher.scala
@@ -3,6 +3,40 @@
   */
 object FileMatcher {
   def main(args: Array[String]) {
+    def containsNeg(nums:List[Int]):Boolean={
+      var exists = false
+      for(num <- nums)
+        if(num<0)
+          exists = true
+      exists
+    }
+    containsNeg(List(1,2,3,4))
+    containsNeg(List(1,-2,3,4))
+    def containsNeg2(nums:List[Int]) = nums.exists(_<0)
+    def containsOdd(nums:List[Int]):Boolean = {
+      var exists = false
+      for(num <- nums)
+        if(num % 2 == 1)
+          exists = true
+      exists
+    }
+    def containsOdd2(nums:List[Int]) =  nums.exists(_ % 2 == 1)
+    def plainOldSum(x:Int,y:Int) = x + y
+    plainOldSum(1,2)
+
+    def curriedSum(x:Int)(y:Int) = x + y
+    curriedSum(1)(2)
+    //实际上接连调用了两个传统的函数，第一个函数调用带单个的名为ｘ的Ｉｎｔ参数，并返回第二个函数的函数值。
+    //第二个函数带Ｉｎｔ参数ｙ
+    def first(x:Int) = (y:Int) => x + y
+
+    val second = first(1)
+    second(2)
+    val onePlus = curriedSum(1) _
+    onePlus(2)
+    val twoPlus = curriedSum(_)(2)
+
+
 
   }
   private def filesHere = (new java.io.File(".")).listFiles
