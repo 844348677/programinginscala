13c2c89c66dd693c39975f0394d730290f5fd738
liuh <844348677@qq.com>
2016-04-18 11:43:35 +0800

    2

---
+++ src/main/scala/impli/MaxListUpBound.scala
@@ -0,0 +1,28 @@
+package impli
+
+/**
+  * Created by liuh on 2016/4/18.
+  */
+object MaxListUpBound {
+  def maxListUpBound[T <: Ordered[T]](element:List[T]):T=
+    element match{
+      case List() =>
+        throw new IllegalArgumentException("empty list!")
+      case List(x) => x
+      case x :: rest =>
+        val maxRest = maxListUpBound(rest)
+        if(x > maxRest) x
+        else maxRest
+    }
+  def maxListImpParm[T](elements:List[T])(implicit ordered:T => Ordered[T]):T=
+  elements match{
+    case List() =>
+      throw new IllegalArgumentException("empty list!")
+    case List(x) => x
+    case x :: rest =>
+      val maxRest = maxListImpParm(rest)(ordered)
+      if(ordered(x) > maxRest) x
+      else maxRest
+  }
+  //def maxListPoorStyle[T](elements:List[T])(implicit ordered:(T,T) => Boolean):T
+}
