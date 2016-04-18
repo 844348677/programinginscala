fe59c40df7a5e1154f2454c2aff544acb417f6df
liuh <844348677@qq.com>
2016-04-18 15:32:45 +0800

    2

---
+++ src/main/scala/loop/Queue.scala
@@ -0,0 +1,26 @@
+package loop
+
+/**
+  * Created by liuh on 2016/4/18.
+  */
+object Queue {
+  def queens(n:Int):List[List[(Int,Int)]]={
+    def placeQueens(k:Int):List[List[(Int,Int)]] =
+      if(k == 0)
+        List(List())
+      else
+        for{
+          queens <- placeQueens(k-1)
+          column <- 1 to n
+          queen = (k,column)
+          if isSafe(queen,queens)
+        } yield queen :: queens
+    placeQueens(n)
+  }
+  def isSafe(queen:(Int,Int),queens:List[(Int,Int)]) =
+    queens forall (q => !inCheck(queen,q))
+  def inCheck(q1:(Int,Int),q2:(Int,Int)) =
+    q1._1 == q2._1 ||
+    q1._2 == q2._2 ||
+      (q1._1 - q2._1).abs == (q1._2 - q2._2).abs
+}
